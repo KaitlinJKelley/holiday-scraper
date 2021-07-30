f62a36c4a716d1f8b1b85a096f42713f2b5f9d8d
kjk1325e <kaitlinjkelley@gmail.comm>
2021-07-30 18:02:57 -0500

    Added Request from request library to include header to avoid 403 error

--- scraper.py
+++ scraper.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
+import urllib
 from bs4 import BeautifulSoup
 
-url = "https://nationaldaycalendar.com/january/"
+url = "https://nationaldaycalendar.com/february/"
 
-page = urlopen(url)
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+
+req = urllib.request.Request(url=url, headers=headers)
+
+page = urlopen(req)
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
