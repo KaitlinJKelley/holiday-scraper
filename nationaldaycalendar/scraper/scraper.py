f44359532fd1956e47023142762a9363fedc5d3f
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-03 17:04:07 -0500

    Refactored to attempt threading; list() not working?

--- nationaldaycalendar/scraper/scraper.py
+++ nationaldaycalendar/scraper/scraper.py
@@ -4,107 +4,125 @@ from bs4 import BeautifulSoup
 from datetime import date
 import calendar
 import re
+import time
+import concurrent.futures
 
-def get_national_days():
+months = list(calendar.month_name)
 
+def get_national_days_for_month(month):
+    months = list(calendar.month_name)
+
+    url = f"https://nationaldaycalendar.com/{month}/"
+
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+
+    req = urllib.request.Request(url=url, headers=headers)
+
+    page = urlopen(req)
+
+    html_bytes = page.read()
+
+    html_decoded = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html_decoded, 'html.parser')
+
+    # Search by ID to get div tags containing national day information
+    div = soup.find(id="et-boc")
+
+    div_text = div.get_text()
+
+    # Remove line breaks
+    text_list = div_text.split('\n')
+
+    # Remove blank spaces left after split
+    days_list = [i for i in text_list if i != '' and i != ' ' and i!= "  "]
+
+    # Removes spaces converted to regex during scrape
+    days_list = [re.sub(r'\xa0', ' ', string) for string in days_list]
+
+    # # There is one double spaced empty string
+    # days_list.remove("  ")
+    # October and December don't have titles at index 0, 
+    # so only pop(0) if the string does not contain a digit from 1 - 31
+    if days_list[0].split(" ")[1].isdigit() == False or int(days_list[0].split(" ")[1]) > 31:
+        days_list.pop(0)
+    
+    return days_list
+
+def get_all_national_days():
     today = date.today()
 
-    if today.month == 8 and today.day == 2:
+    if today.month == 8 and today.day == 3:
 
         months = list(calendar.month_name)
 
         # Removes empty string at beginning of list
         months.pop(0)
 
-        national_days = {}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            national_days = {}
 
-        for month in months:
+            days_lists = executor.map(get_national_days_for_month, months)
 
-            url = f"https://nationaldaycalendar.com/{month}/"
+            for list in days_lists:
+                # Example of final object
+                # {
+                #     month: {
+                #         day of month: [national day, national day]
+                #     }
+                #     month: {
+                #         day of month: [national day, national day]
+                #     }
+                # }
 
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+                # Container for all national days
+                month = months[list]
 
-            req = urllib.request.Request(url=url, headers=headers)
+                national_days[month] = {}
+                
+                for string in days_lists:
+                    try:
+                        day_check = string.split(" ")
 
-            page = urlopen(req)
+                        # August contains a leading space left from replacement of \xa0
+                        if day_check[0] == "":
+                            day_check.pop(0)
 
-            html_bytes = page.read()
+                        day_check = day_check[1]
+        
+                    except:
+                        # For single index holidays which will throw an error on the split
+                        pass
 
-            html_decoded = html_bytes.decode("utf-8")
+                    # Removes suffixes such as st, nd, rd, th from end of numbers (April)
+                    day_check = [i for i in day_check if i.isdigit()]
 
-            soup = BeautifulSoup(html_decoded, 'html.parser')
+                    # Join back remaining digits, if any
+                    day_check = "".join(day_check)
 
-            # Search by ID to get div tags containing national day information
-            div = soup.find(id="et-boc")
+                    try: 
+                        # Convert string to integer
+                        int(day_check)
 
-            div_text = div.get_text()
+                        # If successful, reassign day
+                        day = day_check
 
-            # Remove line breaks
-            text_list = div_text.split('\n')
+                        # Add a list to hold national days on this day in this month
+                        national_days[month][day] = []
+                    except:
+                        # The day didn't change, so add the string onto whatever day we're using
+                        national_days[month][day].append(string)
 
-            # Remove blank spaces left after split
-            days_list = [i for i in text_list if i != '' and i != ' ' and i!= "  "]
-
-            # Removes spaces converted to regex during scrape
-            days_list = [re.sub(r'\xa0', ' ', string) for string in days_list]
-
-            # # There is one double spaced empty string
-            # days_list.remove("  ")
-            # October and December don't have titles at index 0, 
-            # so only pop(0) if the string does not contain a digit from 1 - 31
-            if days_list[0].split(" ")[1].isdigit() == False or int(days_list[0].split(" ")[1]) > 31:
-                days_list.pop(0)
-
-            # Example of final object
-            # {
-            #     month: {
-            #         day of month: [national day, national day]
-            #     }
-            #     month: {
-            #         day of month: [national day, national day]
-            #     }
-            # }
-
-            # Container for all national days
-            national_days[month] = {}
-            
-            for string in days_list:
-                try:
-                    day_check = string.split(" ")
-
-                    # August contains a leading space left from replacement of \xa0
-                    if day_check[0] == "":
-                        day_check.pop(0)
-
-                    day_check = day_check[1]
+            print(national_days)
     
-                except:
-                    # For single index holidays which will throw an error on the split
-                    pass
-
-                # Removes suffixes such as st, nd, rd, th from end of numbers (April)
-                day_check = [i for i in day_check if i.isdigit()]
-
-                # Join back remaining digists, if any
-                day_check = "".join(day_check)
-
-                try: 
-                    # Convert string to integer
-                    int(day_check)
-
-                    # If successful, reassign day
-                    day = day_check
-
-                    # Add a list to hold national days on this day in this month
-                    national_days[month][day] = []
-                except:
-                    # The day didn't change, so add the string onto whatever day we're using
-                    national_days[month][day].append(string)
-
-        return national_days
-
     else:
         # If function is invoked on a date other than the one allowed
         raise Exception("DateError")
 
-get_national_days()
+start = time.perf_counter()
+
+get_all_national_days()
+
+end = time.perf_counter()
+
+print(f"Ran in {end - start} seconds")
