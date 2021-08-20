135876bd35fdd1a375399d8f24e027a23a3a736d
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-20 18:37:36 -0500

    Wrote list function to return all national days

--- nationaldays/views/year.py
+++ nationaldays/views/year.py
@@ -1,5 +1,21 @@
+from django.db.models.base import Model
 from rest_framework.viewsets import ViewSet
+from nationaldays.models.Day import Day
+from rest_framework import serializers, status
+from rest_framework.response import Response
 
 class YearViewSet(ViewSet):
     def list(self, request):
-        pass
+        national_days = Day.objects.all()
+
+        serializer = NationalDaySerializer(national_days ,many=True, context={'request': request})
+
+        return Response(serializer.data)
+
+class NationalDaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Day
+
+        fields = '__all__'
