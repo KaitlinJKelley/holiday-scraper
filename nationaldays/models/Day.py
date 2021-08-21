0dfc38bcc9df977de4a663520ac64937a45c2550
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-21 11:40:05 -0500

    Updated Day model and database

--- nationaldays/models/Day.py
+++ nationaldays/models/Day.py
@@ -1,9 +1,11 @@
-from typing import Text
 from django.db import models
-from django.db.models.fields import CharField, DateField, TextField
+from django.db.models.fields import CharField, IntegerField, TextField
+import datetime
 
 class Day(models.Model):
-    date = DateField(auto_now=False, auto_now_add=False, null=False)
+    month = CharField(max_length=10, default=0)
+    day = IntegerField(default=0)
+    year = IntegerField(default = 0)
     name = CharField(max_length=250, null=False)
     day_history = TextField(null=True)
     day_about = TextField(null=True)
