e4933685231ba0ca0e6f35f8055871fcbeec8915
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-24 07:13:57 -0500

    Reverted database; acessed database to get all national days

--- nationaldays/models/Day.py
+++ nationaldays/models/Day.py
@@ -3,8 +3,8 @@ from django.db import models
 from django.db.models.fields import CharField, DateField, TextField
 
 class Day(models.Model):
-    date = DateField(auto_now=False, auto_now_add=False, null=False)
-    name = CharField(max_length=250, null=False)
+    date = DateField(auto_now=False, auto_now_add=False, null=False, default=None)
+    name = CharField(max_length=250, null=False, default=None)
     day_history = TextField(null=True)
     day_about = TextField(null=True)
     
--- nationaldays/views/year.py
+++ nationaldays/views/year.py
@@ -1,21 +1,70 @@
+from collections import namedtuple
 from django.db.models.base import Model
 from rest_framework.viewsets import ViewSet
 from nationaldays.models.Day import Day
 from rest_framework import serializers, status
 from rest_framework.response import Response
+import psycopg2
+from nationaldays.config.config import config
 
 class YearViewSet(ViewSet):
     def list(self, request):
-        national_days = Day.objects.all()
+    # try:
+        # read connection parameters
+        params = config()
 
-        serializer = NationalDaySerializer(national_days ,many=True, context={'request': request})
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
 
-        return Response(serializer.data)
+        dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        
+        # create a cursor
+        # cursor = conn.cursor()
 
-class NationalDaySerializer(serializers.ModelSerializer):
+        dict_cur.execute("""
+        SELECT id, date, name
+        FROM nationaldays_day 
+        """)
 
-    class Meta:
+        national_days = dict_cur.fetchall()
 
-        model = Day
+        # {
+        #     date: [
+        #         {
+        #             name: name,
+        #             about: about,
+        #             history: history
+        #         }
+        #     ]
+        # }
 
-        fields = '__all__'
+        national_days_list = {}
+
+        for day in national_days:
+            day_object = Day()
+            day_object.date = day.date
+            if day.date in national_days_list:
+                national_days_list[day.date].append({
+                    'name': day.name,
+                    'history': day.day_history,
+                    'about': day.day_about
+                })
+            else:
+                national_days_list[day.date] = []
+
+        print(national_days_list)
+
+        return()
+            
+        # except (Exception, psycopg2.DatabaseError) as error:
+        #     print(error)
+        
+
+
+# class NationalDaySerializer(serializers.ModelSerializer):
+
+#     class Meta:
+
+#         model = Day
+
+#         fields = '__all__'
