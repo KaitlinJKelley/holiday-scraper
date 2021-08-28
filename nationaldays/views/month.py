eb98b7d4844e6df3156c55560bb647d6fcb23f6c
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-28 12:20:49 -0500

    started MonthViewSet; added custom path in urls.py to route pass params and route to function

--- nationaldays/views/month.py
+++ nationaldays/views/month.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 
-class DayViewSet(ViewSet):
-    def list(self, request):
+class MonthViewSet(ViewSet):
+    def day_all(self, month):
         # Returns all national days on a given day of the month
-        pass
+        return Response({'Good':'url'})
     def retrieve(self, request, pk):
         # Returns specific national day
         pass
