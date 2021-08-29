e02f0679b55dc02b57cf7a5e3d9ee6283c46f3e6
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-29 10:02:44 -0500

    Removed unused code

--- nationaldays/views/year.py
+++ nationaldays/views/year.py
@@ -1,5 +1,4 @@
 from django.http.response import HttpResponse
-from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 import psycopg2
